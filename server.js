faa28e6bcb061d3d678e68f550a688ea09c851b5
Grandon Smith <grandon.smith@yahoo.com>
2020-12-06 12:16:50 -0500

    added pokedex data to json file, and implemented authorization checking for api requests

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
+const POKEDEX = require('./pokedex.json')
 
-console.log(process.env.API_TOKEN)
+// console.log(process.env.API_TOKEN)
+
 const app = express();
 app.use(morgan('dev'));
 
@@ -26,16 +28,27 @@ const validTypes = [
     `Steel`, 
     `Water`
     ]
+app.use(function validateBearerToken(req, res, next) {
+    const bearerToken = req.get('Authorization')
+    const apiToken = process.env.API_TOKEN
+    console.log('validate bearer token mid')
+    if(!bearerToken || bearerToken.split(" ")[1] !== apiToken) {
+        return res.status(401).json({error: 'Unauthorized Request'})
+    }
+    next()
+})
+
 function handleGetTypes(req, res) {
     res.json(validTypes)
 }
 app.get('/types', handleGetTypes)
 
+
 function handleGetPokemon(req, res) {
     res.send('Hello Pokemons')
 }
-
 app.get('/pokemon', handleGetPokemon)
+
 
 const port = 8000;
 
